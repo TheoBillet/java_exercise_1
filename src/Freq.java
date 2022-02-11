2bc2b864f33625842a36a7e620b3d8769a08f452
Theo <theo.billet@epita.fr>
2022-02-11 11:38:55 +0100

    Predict

--- src/Freq.java
+++ src/Freq.java
@@ -14,29 +14,49 @@ public class Freq implements  Command {
     public Boolean run(Scanner scanner) {
         System.out.println("Veuillez entrer un chemin vers un fichier :");
         String path = scanner.nextLine();
+        String content = contentFile(path);
+        if (Objects.isNull(content)) {
+            return false;
+        }
+        String[] words = wordsFile(content);
+        List<String> popularWords = mostWordUse(words);
+        System.out.println("Mots les plus utilisés :");
+        popularWords.forEach(word -> {
+            System.out.println(word);
+        });
+        return false;
+    }
+
+    public static String contentFile(String path) {
         Path filePath = Paths.get(path);
         try {
-            String content = Files.readString(filePath);
-            if (content.isBlank()) {
-                System.out.println("Votre fichier est vide !");
-            } else {
-                String[] words = content.replaceAll("[.!?\\-'\"\\n]", " ").toLowerCase().split("\\s");
-                List<String> occ = Arrays.stream(words)
-                        .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
-                        .entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                        .map(Map.Entry::getKey)
-                        .limit(3)
-                        .collect(Collectors.toList());
-                System.out.println("Mots les plus utilisés :");
-                occ.forEach(word -> {
-                    System.out.println(word);
-                });
-            }
+            return Files.readString(filePath);
         }
         catch (Exception e) {
             System.out.println("Unreadable file: " + e.getClass().getSimpleName() + " " + e.getMessage());
+            return null;
         }
-        return false;
+    }
+
+    public static String[] wordsFile(String content) {
+        String[] res = new String[0];
+        if (content.isBlank()) {
+            System.out.println("Votre fichier est vide !");
+            return res;
+        } else {
+            res = content.replaceAll("[,.!?\\-'\"\\n]", " ").toLowerCase().split("\\s");
+            return res;
+        }
+    }
+
+    public static List<String> mostWordUse(String[] words) {
+        List<String> occ = Arrays.stream(words)
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .limit(3)
+                .collect(Collectors.toList());
+        return occ;
     }
 }
---
+++ src/Predict.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Predict implements Command {
+    @Override
+    public String name() {
+        return "predict";
+    }
+
+    @Override
+    public Boolean run(Scanner scanner) {
+        System.out.println("Veuillez entrer un chemin vers un fichier :");
+        String path = scanner.nextLine();
+        String content = Freq.contentFile(path);
+        if (Objects.isNull(content)) {
+            return false;
+        }
+        List<String> predictSentence = new ArrayList<String>();
+        String[] words = Freq.wordsFile(content);
+        System.out.println("Veuillez entrer un mot pour commencer la prédiction :");
+        String wordStart = scanner.nextLine();
+        if (!Arrays.asList(words).contains(wordStart)) {
+            System.out.println("Attention votre mot n'est pas dans votre fichier !");
+            return false;
+        }
+        predictSentence.add(wordStart);
+        while(predictSentence.size() < 20) {
+            String predictWord = predictWord(predictSentence.get(predictSentence.size() - 1), words);
+            if (Objects.isNull(predictWord)) {
+                break;
+            }
+            predictSentence.add(predictWord);
+        }
+        System.out.println("Phrase prédit :");
+        predictSentence.forEach(word -> {
+            System.out.print(word + " ");
+        });
+        System.out.println();
+        return false;
+    }
+    public String predictWord(String word, String[] listWords) {
+        String res;
+        List<String> words = new ArrayList<String>();
+        for (int i = 0; i < listWords.length; i++) {
+            if (listWords[i].equals(word) && (i + 1) != listWords.length) {
+                words.add(listWords[i]);
+            }
+        }
+        String[] arrayWords = new String[words.size()];
+        arrayWords = words.toArray(arrayWords);
+        res = Freq.mostWordUse(arrayWords).get(0);
+        return res;
+    }
+}
